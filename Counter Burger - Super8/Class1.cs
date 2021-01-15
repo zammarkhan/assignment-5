1692d5122a88d35990e16880ec1c419d434050b2
zammarkhan <zammarahmed636@gmail.com>
2021-01-15 22:38:27 +0500

    all project

---
+++ Counter Burger - Super8/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Counter_Burger___Super8
+{
+    public class Class1
+    {
+    }
+}
---
+++ Counter Burger - Super8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Counter_Burger___Super8.Global" Language="C#" %>
